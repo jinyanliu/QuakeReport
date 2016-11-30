12efd6b32e42b457447f797e893ad8e3d0582783
JinyanLiu <jane.jinyan.liu@gmail.com>
2016-11-30 13:45:01 +0100

    Check for connectivity status and handle no internet connect gracefully by showing the text on the screen "No Internet Connect".

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -17,8 +17,11 @@ package com.example.android.quakereport;
 
 import android.app.LoaderManager;
 import android.app.LoaderManager.LoaderCallbacks;
+import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -116,23 +119,41 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderCallb
                 //Convert the String URL into a URI object (to pass into the Intent constructor)
                 Uri earthquakeUri = Uri.parse(currentEarthquake.getUrl());
                 //Create a new intent to view the earthquake URI
-                Intent websiteIntenet = new Intent(Intent.ACTION_VIEW, earthquakeUri);
+                Intent websiteIntent = new Intent(Intent.ACTION_VIEW, earthquakeUri);
                 //Send the intent to launch a new activity
-                startActivity(websiteIntenet);
+                startActivity(websiteIntent);
             }
         });
 
         mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
         earthquakeListView.setEmptyView(mEmptyStateTextView);
 
-        //Geta reference to the LoaderManager, in order to interact with loaders.
-        LoaderManager loaderManager = getLoaderManager();
+        //Get a reference to the ConnectivityManager to check state of network connectivity
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
 
-        //Initialize the loader. Pass in the int ID constant defined above and pass in null for
-        //the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
-        //because this activity implements the LoaderCallbacks interface).
-        loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
-        Log.i(LOG_TAG, "initLoader");
+        //Get details on the currently active default data network
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        //If there is a network connection, fetch data
+        if (networkInfo != null && networkInfo.isConnected()) {
+            //Get a reference to the LoaderManager, in order to interact with loaders.
+            LoaderManager loaderManager = getLoaderManager();
+
+            //Initialize the loader. Pass in the int ID constant defined above and pass in null for
+            //the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+            //because this activity implements the LoaderCallbacks interface).
+            loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
+            Log.i(LOG_TAG, "initLoader");
+        } else {
+            //Otherwise, display error
+            //First, hide loading indicator so error message will be visible
+            View loadingIndicator = findViewById(R.id.loading_spinner);
+            loadingIndicator.setVisibility(View.GONE);
+
+            //Update empty state with no connection error message
+            mEmptyStateTextView.setText(R.string.no_internet);
+        }
 
 //        // Start the AsyncTask to fetch the earthquake data
 //        EarthquakeAsyncTask task = new EarthquakeAsyncTask();

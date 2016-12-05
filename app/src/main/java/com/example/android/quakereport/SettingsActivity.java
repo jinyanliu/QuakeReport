39fa4d4182f8f7b828e5fd5545d357695632dcff
JinyanLiu <jane.jinyan.liu@gmail.com>
2016-12-05 14:53:20 +0100

    Need to update the preference summary when the settings activity is launched. Given the key of a preference, use PreferenceFragment's findPreference() method to get the Preference object, and setup the preference using a helper method called bindPreferenceSummaryToValue().

--- app/src/main/java/com/example/android/quakereport/SettingsActivity.java
+++ app/src/main/java/com/example/android/quakereport/SettingsActivity.java
@@ -1,8 +1,10 @@
 package com.example.android.quakereport;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 
 public class SettingsActivity extends AppCompatActivity {
@@ -18,6 +20,10 @@ public class SettingsActivity extends AppCompatActivity {
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.settings_main);
+
+            Preference minMagnitude = findPreference(getString(R.string.settings_min_magnitude_key));
+            bindPreferenceSummaryToValue(minMagnitude);
+
         }
 
         @Override
@@ -25,6 +31,13 @@ public class SettingsActivity extends AppCompatActivity {
             String stringValue = value.toString();
             preference.setSummary(stringValue);
             return true;
+        }
+
+        private void bindPreferenceSummaryToValue(Preference preference) {
+            preference.setOnPreferenceChangeListener(this);
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+            String preferenceString = preferences.getString(preference.getKey(), "");
+            onPreferenceChange(preference, preferenceString);
         }
 
     }
